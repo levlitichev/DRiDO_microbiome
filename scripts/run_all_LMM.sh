5ca00d6e77ba84374a8819cb1438756636235977
Lev Litichevskiy <levlitichev@gmail.com>
2024-10-24 22:47:19 -0400

    created top-level shell script for running nearly all LMMs

--- scripts/run_all_LMM.sh
+++ scripts/run_all_LMM.sh
@@ -1,5 +1,19 @@
-sh run_all_asreml.sh # slow
+# all ASReml microbiome models
+sh run_all_asreml.sh # slow, cleans up intermediate files
 
-Rscript run_maaslin.R
+# MaAsLin models
+Rscript run_maaslin.R # does not clean up intermediate files
 
+# we use lme4qtl just once for an Extended Data Figure, but it's very slow
 Rscript run_lme4qtl.R # slow
+Rscript collate_lme4qtl.R # does not clean up intermediate files
+
+# microbiome-phenotype associations
+Rscript run_lme4_mb_pheno_assoc.R
+Rscript collate_lme4_mb_pheno_assoc.R
+
+# cross-sectional microbiome-phenotype associations
+# need to run the next script manually since it's .Rmd and not .R
+# lm_cross_sectional_mb_pheno_assoc.Rmd
+
+# N.B. mediation LMMs are run through Snakefile_mediation
